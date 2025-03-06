2e6a80c6b9d0974934c41de4cc8f75ce4b22acc1
vinsmokejazz <vinsmokejazz@gmail.com>
2025-03-06 23:27:51 +0530

    pg upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Route,Routes } from "react-router-dom"
 import Dashboard from "./pages/dashboard"
 import { Signup } from "./pages/Signup"
+import { SignIn } from "./pages/Signin"
 
 function App() {
   
 
   return (
     <>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/" element={<SignIn/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -6,6 +6,7 @@ import { InputBox } from "../components/InputBox";
 export function Signup(){
 return <div className="h-screen w-screen  bg-gray-200 flex justify-center items-center">
    <div className="bg-white rounded-xl border border-slate-300 shadow min-w-48 min-h-42 p-6 ">
+   <p className="flex justify-center text-2xl font-medium p-2">Sign Up</p>
     <InputBox placeholder="Username"/>
     <InputBox placeholder="Password"/>
     <div className="flex justify-center pt-4"><Button variant="primary" text="Sign Up" fullWidth/></div>
